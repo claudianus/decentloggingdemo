9cb82ff0aeefd9f972fa8e0cde4a3b8f995afe6c
Claudianus Mamertus <claudianus@engineer.com>
2018-11-06 16:51:03 +0900

    Add files via upload

---
+++ Logging.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+
+namespace 로깅
+{
+    public class Logging
+    {
+        public enum Level
+        {
+            Log,
+            Info,
+            Warn,
+            Error
+        }
+
+        public string LogFilePath { get; }
+
+        public Logging(string logFilePath)
+        {
+            LogFilePath = logFilePath;
+        }
+
+        public void Log(string message, Level logLevel = Level.Log)
+        {
+            var stackTrace = new StackTrace();
+            var nowTime = DateTime.Now.ToString(CultureInfo.InvariantCulture);
+            var callingMethodName = stackTrace.GetFrame(1).GetMethod().Name;
+            ConsoleColor messageColor = ConsoleColor.Gray;
+
+            switch (logLevel)
+            {
+                case Level.Log:
+                    messageColor = ConsoleColor.White;
+                    message = $"(..Log) {message}";
+                    break;
+                case Level.Info:
+                    messageColor = ConsoleColor.Cyan;
+                    message = $"(.Info) {message}";
+                    break;
+                case Level.Warn:
+                    messageColor = ConsoleColor.Yellow;
+                    message = $"(.Warn) {message}";
+                    break;
+                case Level.Error:
+                    messageColor = ConsoleColor.Red;
+                    message = $"(Error) {message}";
+                    break;
+            }
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write($"[{nowTime}] ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write($"({callingMethodName}) => ");
+            Console.ForegroundColor = messageColor;
+            Console.Write($"{message}");
+            Console.WriteLine();
+
+            var content = $"[{nowTime}] ({callingMethodName}) => {message}";
+            LogOnFile(content);
+        }
+
+        private void LogOnFile(string content)
+        {
+            using (var sw = new StreamWriter("log.txt", true))
+                sw.WriteLine(content);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace 로깅
+{
+    internal static class Program
+    {
+        public static Logging Logger = new Logging("log.log");
+
+        private static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Logger.Log($"{i} 평범한 로그 ㅁㄴㅇㅁ");
+            }
+
+            Logger.Log("정보 ㅁㄴㅇㅁㄴㅇ", Logging.Level.Info);
+            Logger.Log("경고! ㅁㄴㅇㅁ", Logging.Level.Warn);
+            Logger.Log("에러발생! ㅁㄴㅇㅁㄴ", Logging.Level.Error);
+
+            Console.ReadLine();
+        }
+    }
+}
